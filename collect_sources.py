350429d3490da17f00e6a9cf41ecd74cab91a2a2
werener <lenyazhuk@yandex.ru>
2026-03-24 11:35:36 +0300

    feature: create script to collect and print all .cpp files in a certain project

---
+++ collect_sources.py
@@ -0,0 +1,26 @@
+
+import os
+import sys
+from pathlib import Path
+
+def get_sources(app_dir):
+    """Recursively find all .cpp and .c files in the app directory"""
+    app_path = Path(app_dir)
+    src_path = app_path / 'src'
+    
+    sources = []
+    
+    if src_path.exists():
+        for ext in ['*.cpp', '*.c']:
+            sources.extend(src_path.rglob(ext))
+    for source in sources:
+        print(source)
+    
+    return len(sources)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: get_sources.py <app_directory>")
+        sys.exit(1)
+    
+    get_sources(sys.argv[1])
